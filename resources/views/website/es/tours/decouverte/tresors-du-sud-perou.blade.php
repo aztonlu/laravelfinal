f76d8225fbd904752f91a96ed046adcbcfcc48b4
Luis Correa <jluiscorrea90@outlook.com>
2018-05-20 20:36:48 -0500

    prueba
    prueba de despliegues de tags

--- resources/views/website/es/tours/decouverte/tresors-du-sud-perou.blade.php
+++ resources/views/website/es/tours/decouverte/tresors-du-sud-perou.blade.php
@@ -554,42 +554,19 @@ footer{
   <div class="row" style="text-align: center; color: white;">
  
      <div class="col-xs-12 col-md-8">
-      <div class="row" sty>
-        <div class="col-xs-12">
-            <h2><strong>TOURS</strong></h2>
-        </div>
-    
-      </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                   <a href="{{url('')}}/fr/tours/decouverte/Aventure"><h4>Aventure</h4></a>
-              <a href="{{url('')}}/fr/tours/decouverte/Aventure"><img src="{{url('')}}/img/Aventura.jpg" height="60%" width="60%"> 
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                  <a href="{{url('')}}/fr/tours/decouverte/High-Mountain"><h4>Haute Montagne</h4></a>
-            <a href="{{url('')}}/fr/tours/decouverte/High-Mountain"><img src="{{url('')}}/images/Alta_montaña.jpg" height="60%" width="60%" class="img-circle"></a>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                  <a href="{{url('')}}/fr/tours/decouverte/Discover"><h4>Decouverte</h4></a>
-              <a href="{{url('')}}/fr/tours/decouverte/Discover"><img src="{{url('')}}/images/Descubir.jpg" height="60%" width="60%" class="img-circle"></a>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-3">
-                <a href="{{url('')}}/fr/tours/decouverte/Excursions"><h4>Excursions</h4></a>
-                <a href="{{url('')}}/fr/tours/decouverte/Excursions"><img src="{{url('')}}/images/Excursiones.jpg" height="60%" width="60%" class="img-circle"></a>
-            </div>
-        </div>
+      
     </div>
      <div class="col-xs-12 col-md-4">
-            <h2><strong>S'inscrire</strong></h2>
+            <h2><strong>Suscribase</strong></h2>
         
-            <h5 style="color: #fff;">Abonnez-vous pour rester à jour sur les nouvelles et les notifications</h5>
+            <h5 style="color: #fff;">Suscribase para recibir notificaciones</h5>
           
               <form action="{{route('publicnewsletters.store')}}" method="post">
             {{ csrf_field() }}
               <?php $url = Request::path();?>
               <input type="hidden" name="language" value="{{$url}}">
               <input name='email' placeholder='example@mail.com' class="form-control text-center" type="email" style="width:90%">
-              <button type="submit" class="btn-submit text-center" style="width:90%">Inscrivez-vous maintenant</button>
+              <button type="submit" class="btn-submit text-center" style="width:90%">Ingrese su correo</button>
                   
           </form>
            
